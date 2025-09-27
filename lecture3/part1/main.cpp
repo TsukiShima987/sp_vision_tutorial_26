e1ac4c5dd8d58a8ad03e100433bb44dcfb54ee3c
TsukiShima987 <13937178587@163.com>
2025-09-27 16:35:42 +0800

    Update lecture3 classwork

--- lecture3/part1/main.cpp
+++ lecture3/part1/main.cpp
@@ -3,17 +3,26 @@ using namespace std;
 
 class Material
 {
-
+    public:
+        Material(){
+            printf("Material Default Constructor!\n");
+        };
+        void print(){
+            printf("This is a Material Object!\n");
+        };
+        ~Material(){
+            printf("Material Destuctor!\n");
+        };
 };
 
-// int main()
-// {
-//     cout << "--- 构造函数 ---" << endl;
-//     Material m;
+int main()
+{
+    cout << "--- 构造函数 ---" << endl;
+    Material m;
 
-//     cout << "\n--- print函数 ---" << endl;
-//     m.print();
+    cout << "\n--- print函数 ---" << endl;
+    m.print();
 
-//     cout << "\n--- 自动进行析构函数 ---" << endl;
-//     return 0;
-// }
+    cout << "\n--- 自动进行析构函数 ---" << endl;
+    return 0;
+}
--- lecture3/part2/main.cpp
+++ lecture3/part2/main.cpp
@@ -3,17 +3,30 @@ using namespace std;
 
 class Material
 {
-
+public:
+        Material(){
+            printf("Material Default Constructor!\n");
+        };
+        void print(){
+            printf("This is a Material Object!\n");
+            count_++;
+        };
+        ~Material(){
+            printf("Material Destuctor!It has been read %d times\n",count_);
+        };
+private:
+        int count_=0;
 };
 
-// int main()
-// {
-//     cout << "--- 构造函数 ---" << endl;
-//     Material m;
+int main()
+{
+    cout << "--- 构造函数 ---" << endl;
+    Material m;
+    Material n;
 
-//     cout << "\n--- print函数 ---" << endl;
-//     m.print();
+    cout << "\n--- print函数 ---" << endl;
+    m.print();
 
-//     cout << "\n--- 自动进行析构函数 ---" << endl;
-//     return 0;
-// }
+    cout << "\n--- 自动进行析构函数 ---" << endl;
+    return 0;
+}
